7923c29e1a3c155a627754eb42ff9c8a796a0f2e
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-05-19 10:22:09 -0300

    Feature: New Imput component

--- app/lib/components/basics.dart
+++ app/lib/components/basics.dart
@@ -1,8 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class CoreImput extends StatelessWidget {
+class Imput extends StatelessWidget {
+  final TextEditingController _campo = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Text('a');
+    return Padding(
+      padding: const EdgeInsets.only(left: 8),
+      child: TextField(
+        controller: _campo,
+        decoration: InputDecoration(labelText: 'Apelido'),
+        style: TextStyle(fontSize: 24),
+        keyboardType: TextInputType.text,
+      ),
+    );
   }
 }
