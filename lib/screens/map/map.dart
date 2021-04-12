7b1ab90e7bbf4b0792ea2b62e9a4d48d8a253e19
Jeferson Alves <49992182+Viserion77@users.noreply.github.com>
2021-04-12 20:14:28 -0300

    Update map.dart

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,84 +1,23 @@
-// Copyright 2020 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'package:DigitalPark/components/bottomMenuBar.dart';
+import 'package:DigitalPark/screens/map/place_tracker_app.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
-import 'place.dart';
-import 'place_list.dart';
-import 'place_map.dart';
-import 'stub_data.dart';
-
-enum PlaceTrackerViewType {
-  map,
-  list,
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
 }
 
-class PlaceTrackerApp extends StatelessWidget {
+class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: _PlaceTrackerHomePage(),
-    );
-  }
-}
-
-class _PlaceTrackerHomePage extends StatelessWidget {
-  const _PlaceTrackerHomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var state = Provider.of<AppState>(context);
     return Scaffold(
-      body: IndexedStack(
-        index: state.viewType == PlaceTrackerViewType.map ? 0 : 1,
-        children: [
-          PlaceMap(center: const LatLng(45.521563, -122.677433)),
-          PlaceList()
-        ],
+      body: ChangeNotifierProvider(
+        create: (context) => AppState(),
+        child: PlaceTrackerApp(),
       ),
+      bottomNavigationBar: BottomMenuBar(),
     );
   }
-}
-
-class AppState extends ChangeNotifier {
-  AppState({
-    this.places = StubData.places,
-    this.selectedCategory = PlaceCategory.favorite,
-    this.viewType = PlaceTrackerViewType.map,
-  })  : assert(places != null),
-        assert(selectedCategory != null);
-
-  List<Place> places;
-  PlaceCategory selectedCategory;
-  PlaceTrackerViewType viewType;
-
-  void setViewType(PlaceTrackerViewType viewType) {
-    this.viewType = viewType;
-    notifyListeners();
-  }
-
-  void setSelectedCategory(PlaceCategory newCategory) {
-    selectedCategory = newCategory;
-    notifyListeners();
-  }
-
-  void setPlaces(List<Place> newPlaces) {
-    places = newPlaces;
-    notifyListeners();
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is AppState &&
-        other.places == places &&
-        other.selectedCategory == selectedCategory &&
-        other.viewType == viewType;
-  }
-
-  @override
-  int get hashCode => hashValues(places, selectedCategory, viewType);
 }
