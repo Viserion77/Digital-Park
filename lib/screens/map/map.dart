e27cff20b902971efb7272de20f8c27bb1b64830
Jeferson Alves <49992182+Viserion77@users.noreply.github.com>
2021-04-28 20:37:46 -0300

    Update map.dart

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -2,6 +2,7 @@ import 'package:DigitalPark/components/bottomMenuBar.dart';
 import 'package:DigitalPark/screens/map/place_tracker_app.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 class Map extends StatefulWidget {
@@ -10,12 +11,28 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  final Set<Marker> _markers = {};
   @override
   Widget build(BuildContext context) {
+    _markers.add(Marker(
+      markerId: MarkerId("1"),
+      position: LatLng(-26.507562218125386, -49.12852719426155),
+      infoWindow: InfoWindow(
+        title: "teste 1",
+      ),
+      icon: BitmapDescriptor.defaultMarker,
+      visible: true,
+    ));
     return Scaffold(
       body: ChangeNotifierProvider(
         create: (context) => AppState(),
-        child: PlaceTrackerApp(),
+        child: GoogleMap(
+          markers: _markers,
+          initialCameraPosition: CameraPosition(
+            target: const LatLng(-26.507562218125386, -49.12852719426155),
+            zoom: 11.0,
+          ),
+        ),
       ),
       bottomNavigationBar: BottomMenuBar(functionIcon: Icons.search),
     );
