53d6ed1f340ea30398673906c7b5c062446aa01b
Jeferson Alves <jeferson.a.oficial@gmail.com>
2021-11-15 22:16:33 -0300

    Update default_scaffold_app.dart

--- lib/components/default_scaffold_app.dart
+++ lib/components/default_scaffold_app.dart
@@ -3,6 +3,7 @@ import 'package:digital_park/components/side_menu.dart';
 import 'package:digital_park/models/user/user_profile.dart';
 import 'package:digital_park/route_generator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DefaultScaffoldApp extends StatelessWidget {
   const DefaultScaffoldApp({
@@ -16,56 +17,66 @@ class DefaultScaffoldApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: NavDrawer(
-        userProfile: userProfile,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent, //i like transaparent :-)
+        systemNavigationBarColor:
+            Theme.of(context).secondaryHeaderColor, // navigation bar color
+        statusBarIconBrightness: Brightness.dark, // status bar icons' color
+        systemNavigationBarIconBrightness:
+            Brightness.dark, //navigation bar icons' color
       ),
-      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: Container(
-        decoration: const BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.all(
-            Radius.circular(100),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.white,
-              spreadRadius: 1.5,
-              blurRadius: 1.5,
-              offset: Offset(0.5, 0.5),
-            ),
-          ],
-        ),
-        child: SizedBox(
-          width: 85,
-          height: 85,
-          child: FittedBox(
-            child: FloatingActionButton(
-              onPressed: () => navigatorRoute(
-                context,
-                '/map',
-                arguments: userProfile,
-              ),
-              child: const Icon(
-                Icons.map,
-                size: 35.0,
-              ),
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        notchMargin: 3.5,
-        child: BottomMenuBar(
+      child: Scaffold(
+        drawer: NavDrawer(
           userProfile: userProfile,
         ),
-        elevation: 3.0,
-      ),
-      extendBody: true,
-      body: SafeArea(
-        bottom: false,
-        child: body,
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        floatingActionButton: Container(
+          decoration: const BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.all(
+              Radius.circular(100),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                spreadRadius: 1.5,
+                blurRadius: 1.5,
+                offset: Offset(0.5, 0.5),
+              ),
+            ],
+          ),
+          child: SizedBox(
+            width: 85,
+            height: 85,
+            child: FittedBox(
+              child: FloatingActionButton(
+                onPressed: () => navigatorRoute(
+                  context,
+                  '/map',
+                  arguments: userProfile,
+                ),
+                child: const Icon(
+                  Icons.map,
+                  size: 35.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          notchMargin: 3.5,
+          child: BottomMenuBar(
+            userProfile: userProfile,
+          ),
+          elevation: 3.0,
+        ),
+        extendBody: true,
+        body: SafeArea(
+          bottom: false,
+          child: body,
+        ),
       ),
     );
   }
